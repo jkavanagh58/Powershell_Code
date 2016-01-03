3e4f3a30ee25311f89f4ca61e743818dfd442e60
John J Kavanagh <john.kavanagh@outlook.com>
2016-01-02 21:05:27 -0500

    SCCM Client Repair
    Code I used to create a localized version

---
+++ scratch-vmowner.ps1
@@ -0,0 +1,5 @@
+﻿$vms = "is1ops04","is1ops05","sc1ops01","sc1ops02"
+foreach ($vm in $vms){
+    $varOwner = get-vm $vm | get-annotation -customattribute "Owner"
+    "Virtual Machine {0} currently shows an owner of {1}" -f $vm, $varOwner.value
+}
---
+++ scratchpad-activesync.ps1
@@ -0,0 +1,9 @@
+﻿if ((get-adserversettings).ViewEntireForest -eq $False){
+    Set-ADServerSettings -ViewEntireForest $true
+}
+$eas = Get-CASMailbox -ResultSize Unlimited | 
+    where { $_.HasActiveSyncDevicePartnership -eq $True }
+# Find activesync devices that have been inactive for 180 or greater days
+$del = $eas | ForEach-Object { Get-ActiveSyncDeviceStatistics -Mailbox $_.Identity | where { $_.LastSyncAttempttime -lt (Get-date).adddays(-180) }}
+# Remove the activesync device partnership for the inactive devices
+$del | % {Remove-ActiveSyncDevice $_.identity -Confirm:$False -Whatif }

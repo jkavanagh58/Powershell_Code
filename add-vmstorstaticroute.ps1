dba809b43025fd63cd0a4f4ab3a1306388d87d5e
John J Kavanagh <john.kavanagh@kavanaghtech.net>
2016-03-03 13:38:22 -0500

    Add-vmstaticroute
    Code used to connect ESXi hosts to Windows Server NFS Share :computer:

---
+++ add-vmstorstaticroute.ps1
@@ -0,0 +1,7 @@
+﻿# Quick script to push the vmstor static route after Puskar made changes
+$vmhosts = get-vmhost          # Process all ESX hosts across datacenters
+$vmstorip = "10.151.64.32"     # New IP assigned by Puskar
+ForEach ($vmhost in $vmhosts){
+# Process two entries for each host
+    New-VMHostRoute -vmhost $vmhost -Destination $vmstorip -Gateway 10.217.25.1 -PrefixLength 32 -Confirm:$false 
+} # Finish processing hosts
